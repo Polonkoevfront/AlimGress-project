3caf8b0b83ce043090906a0b57efde386eb28e1e
Anonymous <abdulmalikpolonkoev4@gmail.com>
2025-05-12 14:40:00 +0300

    the 3-rd commit

---
+++ project/src/App/provider/ui/Routing/Routing.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+
+export const Routing = () => {
+  return (
+    <>
+    <Routes>
+
+      {/* <Route path="/" ele/> */}
+
+    </Routes>      
+    </>
+  )
+}
+
