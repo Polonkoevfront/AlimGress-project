8870dd79d508ac5c619be855a617d18efe4ad7b7
Anonymous <abdulmalikpolonkoev4@gmail.com>
2025-05-15 12:02:35 +0300

    the 7-th commit

--- project/src/App/shared/ui/Button/Button.tsx
+++ project/src/App/shared/ui/Button/Button.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react'
 import cls from './Button.module.scss'
 import { classNames, Mods } from '../../lib/classNames';
 
-type ButtonVariant =  "default" | "primary";  
+type ButtonVariant = "default" | "primary";  
 type ButtonSize = 42 | 70 | 83 | 91 | 113 | 115 | 135 | 169 | 175 | 212;
 
 interface ButtonProps {
@@ -19,16 +19,16 @@ const variantClasses: Record<ButtonVariant, string> = {
 }
 
 const sizeClasses: Record<ButtonSize, string> = {
-    "42": cls.size42,
-    "70": cls.size70,
-    "83": cls.size83,
-    "91": cls.size91,
-    "113": cls.size113,
-    "115": cls.size115,
-    "135": cls.size135,
-    "169": cls.size169,
-    "175": cls.size175,
-    "212": cls.size212,
+    42: cls.size42,
+    70: cls.size70,
+    83: cls.size83,
+    91: cls.size91,
+    113: cls.size113,
+    115: cls.size115,
+    135: cls.size135,
+    169: cls.size169,
+    175: cls.size175,
+    212: cls.size212,
 }
 
 export const Button: FC<ButtonProps> = (props) => {
