a1b7f505c840bae442774353500a63d389eac5c2
Anonymous <abdulmalikpolonkoev4@gmail.com>
2025-05-16 14:49:55 +0300

    the 8-th commit

---
+++ project/src/App/shared/ui/Form/Form.tsx
@@ -0,0 +1,13 @@
+import { FormHTMLAttributes } from 'react'
+import cls from './Form.module.scss'
+
+interface FormatProps {}
+
+export const Form = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
