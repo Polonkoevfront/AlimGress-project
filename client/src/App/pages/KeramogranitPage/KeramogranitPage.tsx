d4cad8c5b0f0cb17987843d51101990c10e703c7
Anonymous <abdulmalikpolonkoev4@gmail.com>
2025-05-22 16:28:01 +0300

    the 14-th commit

---
+++ client/src/App/pages/KeramogranitPage/KeramogranitPage.tsx
@@ -0,0 +1,20 @@
+import { Text } from '../../shared'
+import { Footer, Navbar } from '../../widgets'
+import cls from './KeramogranitPage.module.scss'
+
+export function KeramogranitPage() {
+  return (
+    <div>
+      <div>
+        <Navbar/>
+
+        <div>
+            <Text>Керамогранит</Text>
+        </div>
+
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
